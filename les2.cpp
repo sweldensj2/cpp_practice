35d7e860f3d0428040c5eb4442eb7b79b9bb9f64
sweldensj2 <jws2215@columbia.edu>
2025-01-09 13:14:26 -0500

    basic math with input and output

---
+++ les2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double sales = 95000;
+    cout << "Sales: $" << sales << endl;
+
+    const double stateTaxRate = 0.04;
+    double stateTax = sales * stateTaxRate;
+    cout << "State Tax: $" << stateTax << endl;
+
+    const double countyTaxRate = 0.02;
+    double countyTax = sales * countyTaxRate;
+    cout << "County Tax: $" << countyTax << endl;
+
+    double totalTax = stateTax + countyTax;
+    cout << "Total Tax: $" << totalTax << endl;
+
+    return 0;
+}
---
+++ les3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter two doubles: " << endl;
+    double x;
+    double y;
+    cin >> x;
+    cin >> y;
+    cout << "X + Y = " << (x+y) << endl;
+
+    return 0;
+}
