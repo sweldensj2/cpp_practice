152133c9e65f7c7e01811dc5eed01da97bee07a6
sweldensj2 <jws2215@columbia.edu>
2025-01-09 12:53:00 -0500

    spacing

---
+++ les1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int file_size;
+    file_size = 100;
+
+    double sales = 9.99;
+
+    std::cout << "Tested\n";
+    std::cout << file_size << "\n";
+    std::cout << sales << "\n";
+
+    return 0;
+}
